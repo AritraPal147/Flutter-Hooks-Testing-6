967c439d4dddb26aef5bccf95bc97f76f3d23d90
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-21 12:25:47 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,15 +14,51 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+const url = 'https://bit.ly/3x7J5Qt';
+
+class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    late final StreamController<double> controller;
+
+    controller = useStreamController<double>(
+      /// Set the initial default value of controller to zero,
+      /// i.e., the rotation angle of the image is initially zero.
+      onListen: () => controller.sink.add(0.0),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
+      body: StreamBuilder<double>(
+          stream: controller.stream,
+          builder: (context, snapshot) {
+            /// Checks if snapshot of stream does not have data
+            /// If snapshot does not have data, a CircularProgressIndicator is shown.
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            } else {
+              // If snapshot has data, the data is stored into variable [rotation].
+              final rotation = snapshot.data ?? 0.0;
+              return GestureDetector(
+                /// Increases rotation value by 10 every time the image is tapped on.
+                onTap: () {
+                  controller.sink.add(rotation + 10.0);
+                },
+                child: RotationTransition(
+                  /// The value required by AlwaysStoppedAnimation is in radians, so
+                  /// we divide the current value of rotation by 360
+                  turns: AlwaysStoppedAnimation(rotation / 360),
+                  child: Center(
+                    child: Image.network(url),
+                  ),
+                ),
+              );
+            }
+          }),
     );
   }
 }
